12d783fc635bafa9754cbc33317cd5a470955bf9
SVeeIS <vionaindah11@gmail.com>
2021-05-23 11:18:50 +0800

    KuisCoding

---
+++ KuisCoding/VariabeldanTipeData/main.js
@@ -0,0 +1,28 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+// TODO
+let firstName = 'Viona';
+let lastName = 'Indah';
+let age = 21;
+let isMarried = false;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(isMarried);
+            
+
+/**
+ * Hiraukan kode di bawah ini
+ */
+
+module.exports = {
+  firstName, lastName, age, isMarried,
+};
